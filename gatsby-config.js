39a41f2d5d9dcb94516ca247171a09ffbaee35af
Alvaro Leal <pursuance@gmail.com>
2019-10-19 18:10:16 +0200

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,13 +38,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `bad-possum`,
+        short_name: `bad-possum`,
         start_url: `/`,
         background_color: `#000`,
         theme_color: `#ccc`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
